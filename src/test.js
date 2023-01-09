e2e1ac9b725119b651a9a985fb0c61e19ebce8ab
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2023-01-09 16:36:46 +0200

    Monsters project is done

---
+++ src/test.js
@@ -0,0 +1,14 @@
+
+
+
+
+/*then((data) =>{
+  then(res=>res.json()).
+  then(data=>{
+    this.setState(() => {
+      return{
+        monsters:data
+      }
+    }, ()=>{
+      console.log(this.state.monsters)
+    })}).catch(err=>console.log('failed'));*/
